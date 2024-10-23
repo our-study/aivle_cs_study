dacb150f6994d6013244475eda0a9965313f1332
Younghyun SEO <tigerseo7686@gmail.com>
2024-10-23 22:45:20 +0900

    서영현:0주차 테스트

--- commitlint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    rules: {
-      'type-enum': [2, 'always', ['서영현', '조영진', '김예지', '석은규', '라대경', '서민석']],
-      'header-pattern': [2, 'always', /^(서영현|조영진|김예지|석은규|라대경|서민석)\s?:\s?[0-9]{1,2}주차\s?.{0,50}$/],
-      'subject-min-length': [2, 'always', 1],  // 최소 1글자 이상
-      'subject-max-length': [2, 'always', 50]  // 최대 50글자 이하
-    }
-  };
-  
